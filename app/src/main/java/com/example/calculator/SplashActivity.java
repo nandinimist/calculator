04e7e81f1224a3e849604f1f6ace09aef4d3b6f3
nandinimist <mistrynandini1024@gmail.com>
2023-11-19 14:08:48 -0800

    Calculator version 2.0

--- app/src/main/java/com/example/calculator/SplashActivity.java
+++ app/src/main/java/com/example/calculator/SplashActivity.java
@@ -1,7 +1,13 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.calculator.MainActivity;
+import com.example.calculator.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // hides action bar on launch screen only.
+        getSupportActionBar().hide();
+
+        // directs user to main activity after displaying splash screen.
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },1000);
+
     }
 }
